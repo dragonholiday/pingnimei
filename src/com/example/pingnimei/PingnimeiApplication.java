6ff3f6cc649e5dff7000a869d515f0f1ce45716b
Zhang Xianglong <zhangxianglong@tp-link.net>
2014-08-25 10:29:36 +0800

    Test Baidu LBS SDK

--- src/com/example/pingnimei/PingnimeiApplication.java
+++ src/com/example/pingnimei/PingnimeiApplication.java
@@ -1,8 +1,12 @@
 package com.example.pingnimei;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 
 public class PingnimeiApplication extends Application {
 
@@ -14,6 +18,20 @@ public class PingnimeiApplication extends Application {
 
 		NetworkManager.getInstance(this);
 		mLocationClient = new LocationClient(this);
+        mLocationClient.registerLocationListener(mLocationListener);
+        LocationClientOption option = new LocationClientOption();
+//        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(4000);
+        mLocationClient.setLocOption(option);
+        mLocationClient.start();
 	}
+
+    private BDLocationListener mLocationListener = new BDLocationListener() {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            Log.d("test", "经度:" + location.getLongitude() + ", 纬度:" + location.getLatitude());
+        }
+    };
 
 }
